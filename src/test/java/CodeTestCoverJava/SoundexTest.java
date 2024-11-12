0f3d94fdf53ceefac2c0ec73b2c16cc913d7cd10
ChaithanyaChatra <chaithanyagchatra@gmail.com>
2024-11-12 11:59:56 +0530

    Update SoundexTest.java

--- src/test/java/CodeTestCoverJava/SoundexTest.java
+++ src/test/java/CodeTestCoverJava/SoundexTest.java
@@ -1,6 +1,6 @@
 package CodeTestCoverJava;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -10,26 +10,46 @@ public class SoundexTest {
     public void testEmptyString() {
         assertEquals("", Soundex.generateSoundex(""));
     }
-    
+
     @Test
-    public void testNullInput() {
+    public void testNullString() {
         assertEquals("", Soundex.generateSoundex(null));
     }
 
     @Test
     public void testSingleCharacter() {
-    assertEquals("A000", Soundex.generateSoundex("A"));    
+        assertEquals("A000", Soundex.generateSoundex("A"));
     }
 
     @Test
-    public void testNormalNames() {
-        assertEquals("A530", Soundex.generateSoundex("Ashcraft"));
-        assertEquals("C520", Soundex.generateSoundex("Carpenter"));
+    public void testVowels() {
+        assertEquals("A000", Soundex.generateSoundex("AEIOU"));
     }
-    
+
     @Test
-    public void testPaddingZeros() {
-        assertEquals("M650", Soundex.generateSoundex("Miller")); 
-        assertEquals("T400", Soundex.generateSoundex("Tarek")); 
+    public void testConsonants() {
+        assertEquals("S530", Soundex.generateSoundex("Smith")); // S530
+        assertEquals("R163", Soundex.generateSoundex("Robert")); // R163
+    }
+
+    @Test
+    public void testRepeatedConsonants() {
+        assertEquals("T315", Soundex.generateSoundex("Ttfn")); // T315
+    }
+
+    @Test
+    public void testPadding() {
+        assertEquals("W623", Soundex.generateSoundex("Wright")); // W623
+        assertEquals("P620", Soundex.generateSoundex("Park"));   // Updated to P620
+    }
+
+    @Test
+    public void testMixedCase() {
+        assertEquals("R163", Soundex.generateSoundex("RoBeRt")); // R163
+    }
+
+    @Test
+    public void testHAndWHandling() {
+        assertEquals("R000", Soundex.generateSoundex("RWH")); // R000
     }
 }
